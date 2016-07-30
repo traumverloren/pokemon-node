5f4bdd67ad9cb2253bdec1fd46d9b65557b7625c
Stephanie Nemeth <traumverloren@gmail.com>
2016-07-30 13:23:04 +0200

    initial commit, setup express server

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json({}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/:address', (req, res) => {
+  res.type('text/plain').send(`Ahoy! ${req.params.address}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
