62f78c1896ecbe2aedf01fc220084d6fdfc13469
Stephanie Nemeth <traumverloren@gmail.com>
2016-07-30 18:55:26 +0200

    add duration in human time with moment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const Pokespotter = require('pokespotter');
+const moment = require('moment');
 const app = express();
 
 app.use(bodyParser.json({}))
@@ -17,9 +18,18 @@ const PORT = process.env.PORT || 3000;
 const POKEDEX = Pokespotter.Pokedex;
 const spotter = Pokespotter();
 
+function enhancePokeInfo(pokemon) {
+    pokemon.duration = moment.duration(pokemon.expirationTime - Date.now()).humanize();
+    return pokemon;
+}
+
+function sortClosestPokemon(pokemonA, pokemonB) {
+  return pokemonA.distance - pokemonB.distance;
+}
+
 function getPokemonByAddress(address) {
   return spotter.getNearby(address, { steps: 2, requestDelay: 75 }).then(pokemon => {
-    return pokemon;
+    return pokemon.map(enhancePokeInfo).sort(sortClosestPokemon)
   });
 }
 
