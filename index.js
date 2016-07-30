7ee7b8a7f0f8391630503f5f2d529e884dea6aa0
Stephanie Nemeth <traumverloren@gmail.com>
2016-07-30 16:35:56 +0200

    api call with address works

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const Pokespotter = require('pokespotter');
-
 const app = express();
 
 app.use(bodyParser.json({}))
@@ -14,12 +14,22 @@ app.use(bodyParser.urlencoded({
 
 const PORT = process.env.PORT || 3000;
 
-
 const POKEDEX = Pokespotter.Pokedex;
-const spotter = Pokespotter(process.env.PGO_USERNAME, process.env.PGO_PASSWORD, process.env.PGO_PROVIDER);
+const spotter = Pokespotter();
+
+function getPokemonByAddress(address) {
+  return spotter.getNearby(address, { steps: 2, requestDelay: 75 }).then(pokemon => {
+    return pokemon;
+  });
+}
 
 app.get('/:address', (req, res) => {
-  res.type('text/plain').send(`Ahoy! ${req.params.address}`);
+  getPokemonByAddress(req.params.address).then(pokemon => {
+    res.type('text/plain').send(pokemon.map(function (p) { return JSON.stringify(p); }).toString());
+  }).catch(err => {
+    res.type('text/plain').status(500).send('An error occurred. Check your console.');
+    console.error(err);
+  });
 });
 
 app.listen(PORT, () => {
